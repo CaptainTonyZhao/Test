9877208f3a3a0b6a1ca170ad58ab8e466e8deb35
zhaolei <zhaolei@czhld.com>
2024-11-15 19:05:12 +0800

    dev的第3次提交

--- src/helloworld/cs.java
+++ src/helloworld/cs.java
@@ -1,9 +1,9 @@
 package helloworld;
 
 public class cs {
-    String name = "C#";
+    String hello = "C#";
 
     String greet() {
-        return "Hello, " + name + "!";
+        return "Hello, " + hello + "!";
     }
 }
